829bd652c8a64c0f2a775956fe241402ab2ac908
smbarajas <32750643+smbarajas@users.noreply.github.com>
2019-05-29 17:35:32 -0700

    Create Assigment7.java

---
+++ Assigment7.java
@@ -0,0 +1,22 @@
+/* 
+Wrote a modified version of the selection sort algorithm that selects the largest element each time 
+and moves it to the end of the array, instead of selecting the smallest element and moving it towards the beginning.
+*/
+
+int i, j, maxNum, maxInde, temp = 0;
+for (i = list.length-1; i >= 0; i--) {
+
+    maxNum = list[i];
+    maxInde = i;
+    for (j = 0; j < i; j++) { 
+        if (list[j] > maxNum) {
+            maxNum = list[j];
+            maxInde = j;
+        }
+    }
+    if (maxNum > list[i]) {
+        temp = list[i];
+        list[i] = list[maxInde];
+        list[maxInde] = temp;
+    }
+}
